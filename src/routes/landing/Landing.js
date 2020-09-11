358dfd73738714bb7a0a98d5bf0744f6bc585a2a
George Brown <sumthnabtgeorge@gmail.com>
2020-09-11 14:04:06 -0400

    edited message in landing.js

--- src/routes/landing/Landing.js
+++ src/routes/landing/Landing.js
@@ -7,7 +7,7 @@ export default function Landing() {
 		<div className='landing__container'>
 			<h1 className='landing__header'>BuddyUp</h1>
 			<h2 className='landing__header2'>Welcome To BuddyUp!</h2>
-			<p className='landing__message'>BuddyUp is an application that allows you to organize and create activities in your area. Meet new friends while doing activies you enjoy with BuddyUp! Click below to get started.</p>
+			<p className='landing__message'>BuddyUp is an application that allows you to organize and create activities in your area. Meet new friends while doing the activies you enjoy with BuddyUp! Click below to get started.</p>
 			<button type='button'>
 				<Link to='/register'>Get Started!</Link>
 			</button>
