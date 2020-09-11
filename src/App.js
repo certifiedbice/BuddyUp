976261e98752813d2cf78b221ce599ee1dd98a1d
Matt <63121708+themlp101@users.noreply.github.com>
2020-09-11 14:07:43 -0400

    landing change on app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import './App.css';
-import Login from './routes/login/Login';
-import { Switch, Route } from 'react-router';
-import Register from './routes/register/Register';
-import Dashboard from './routes/dashboard/Dashboard';
-import Demo from './routes/demo/Demo';
+import React from 'react'
+import './App.css'
+import Login from './routes/login/Login'
+import { Switch, Route } from 'react-router'
+import Register from './routes/register/Register'
+import Dashboard from './routes/dashboard/Dashboard'
+import Demo from './routes/demo/Demo'
+import Landing from './routes/landing/Landing'
 
 function App() {
 	return (
 		<div className='App'>
 			<Switch>
-				<Route exact path='/' component={Demo} />
+				<Route exact path='/' component={Landing} />
 				<Route path='/login' component={Login} />
 				<Route path='/register' component={Register} />
 				<Route path='/dashboard' component={Dashboard} />
 			</Switch>
 		</div>
-	);
+	)
 }
 
-export default App;
+export default App
--- src/routes/landing/Landing.js
+++ src/routes/landing/Landing.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 import './Landing.css'
 
 export default function Landing() {
@@ -7,10 +7,15 @@ export default function Landing() {
 		<div className='landing__container'>
 			<h1 className='landing__header'>BuddyUp</h1>
 			<h2 className='landing__header2'>Welcome To BuddyUp!</h2>
-			<p className='landing__message'>BuddyUp is an application that allows you to organize and create activities in your area. Meet new friends while doing the activies you enjoy with BuddyUp! Click below to get started.</p>
+			<p className='landing__message'>
+				BuddyUp is an application that allows you to organize
+				and create activities in your area. Meet new friends
+				while doing the activies you enjoy with BuddyUp! Click
+				below to get started.
+			</p>
 			<button type='button'>
 				<Link to='/register'>Get Started!</Link>
 			</button>
 		</div>
-	);
+	)
 }
