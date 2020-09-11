fb4f622d6704a89baa06ba581677b64102f595d1
George Brown <sumthnabtgeorge@gmail.com>
2020-09-11 14:07:31 -0400

    re-commit for all files

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import Login from './routes/login/Login';
 import { Switch, Route } from 'react-router';
 import Register from './routes/register/Register';
 import Dashboard from './routes/dashboard/Dashboard';
-import Demo from './routes/demo/Demo';
+import Landing from './routes/landing/Landing';
 
 function App() {
 	return (
 		<div className='App'>
 			<Switch>
-				<Route exact path='/' component={Demo} />
+				<Route exact path='/' component={Landing} />
 				<Route path='/login' component={Login} />
 				<Route path='/register' component={Register} />
 				<Route path='/dashboard' component={Dashboard} />
