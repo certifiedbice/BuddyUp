010e3480fe33c35b49f5216c93101062168d74c0
Matt <63121708+themlp101@users.noreply.github.com>
2020-09-11 14:00:22 -0400

    pre dashboard

--- src/routes/dashboard/Dashboard.js
+++ src/routes/dashboard/Dashboard.js
@@ -1,5 +1,133 @@
-import React from 'react';
+import React from 'react'
+import './Dashboard.css'
 
 export default function Dashboard() {
-	return <main>This is a dashboard</main>;
+	return (
+		<main>
+			<header className='dashboard__header'>
+				<h1>BuddyUp</h1>
+				<h2>Find or create your next event</h2>
+				<div>x number of events in your area</div>
+			</header>
+			<section className='event__section'>
+				<ul className='event__list'>
+					<li>
+						<div className='event__item'>
+							<div className='date__container'>
+								<h3>9/11/2020</h3>
+							</div>
+							<div className='info__container'>
+								<h5>Swing</h5>
+								<h6>disco dancing with me</h6>
+							</div>
+							<div className='info__button'>
+								<button>INFO</button>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div className='event__item'>
+							<div className='date__container'>
+								<h3>9/11/2020</h3>
+							</div>
+							<div className='info__container'>
+								<h5>Funeral</h5>
+								<h6>
+									Come with me to my cousins'
+									friend's uncle's funeral
+								</h6>
+							</div>
+							<div className='info__button'>
+								<button>INFO</button>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div className='event__item'>
+							<div className='date__container'>
+								<h3>9/11/2020</h3>
+							</div>
+							<div className='info__container'>
+								<h5>Title</h5>
+								<h6>Description</h6>
+							</div>
+							<div className='info__button'>
+								<button>INFO</button>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div className='event__item'>
+							<div className='date__container'>
+								<h3>9/11/2020</h3>
+							</div>
+							<div className='info__container'>
+								<h5>Title</h5>
+								<h6>Description</h6>
+							</div>
+							<div className='info__button'>
+								<button>INFO</button>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div className='event__item'>
+							<div className='date__container'>
+								<h3>9/11/2020</h3>
+							</div>
+							<div className='info__container'>
+								<h5>Title</h5>
+								<h6>Description</h6>
+							</div>
+							<div className='info__button'>
+								<button>INFO</button>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div className='event__item'>
+							<div className='date__container'>
+								<h3>9/11/2020</h3>
+							</div>
+							<div className='info__container'>
+								<h5>Title</h5>
+								<h6>Description</h6>
+							</div>
+							<div className='info__button'>
+								<button>INFO</button>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div className='event__item'>
+							<div className='date__container'>
+								<h3>9/11/2020</h3>
+							</div>
+							<div className='info__container'>
+								<h5>Title</h5>
+								<h6>Description</h6>
+							</div>
+							<div className='info__button'>
+								<button>INFO</button>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div className='event__item'>
+							<div className='date__container'>
+								<h3>9/11/2020</h3>
+							</div>
+							<div className='info__container'>
+								<h5>Title</h5>
+								<h6>Description</h6>
+							</div>
+							<div className='info__button'>
+								<button>INFO</button>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</section>
+		</main>
+	)
 }
