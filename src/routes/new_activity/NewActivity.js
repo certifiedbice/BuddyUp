f5f00f4e31b7a4c35eca7075a2c917c6a757d3f8
Matt <63121708+themlp101@users.noreply.github.com>
2020-09-15 16:24:47 -0400

    date form input testing

--- src/routes/new_activity/NewActivity.js
+++ src/routes/new_activity/NewActivity.js
@@ -1,29 +1,24 @@
-import React, { useState } from 'react'
+import React from 'react'
+import useForm from '../../hooks/useForm'
+import './NewActivity.css'
 
 export default function NewActivity() {
 	let today = new Date()
 	let dString = today.toISOString().slice(0, 16)
-	const [date, setDate] = useState({})
 
+	const { handleChange, values } = useForm({ date: dString })
 	const handleSubmit = (e) => {
 		e.preventDefault()
-		const { user_date } = e.target
-
-		setDate({ date: user_date.value })
-
-		let newDate = { date: user_date.value }
-		console.log(e.target, newDate, date)
+		console.log(values)
 	}
-	const handleChange = (e) => {
-		setDate(e.target.value)
-	}
+
 	return (
 		<div>
-			<form onSubmit={handleSubmit}>
+			<form id='date__form' onSubmit={handleSubmit}>
 				<input
-					name='user_date'
+					name='date'
 					type='datetime-local'
-					defaultValue={dString}
+					value={values.date}
 					onChange={handleChange}
 				/>
 				<button type='submit'>Submit</button>
